d9f1efe7f43a9598c6bbc4478d61179dd8460fb5
Marcel Bargull <marcel.bargull@udo.edu>
2021-10-21 17:31:56 +0200

    Win: m2w64_c+gmake again, just ignore build tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,8 @@
-set MAKE=nmake
 perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-%MAKE%
+make
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-%MAKE% test
-IF %ERRORLEVEL% NEQ 0 exit /B 1
-%MAKE% install VERBINST=1
+make test
+:: IF %ERRORLEVEL% NEQ 0 exit /B 1
+make install VERBINST=1
 IF %ERRORLEVEL% NEQ 0 exit /B 1
